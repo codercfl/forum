646ebb94e2cc840005ea056c744c9c66e0f2a032
codercfl <2504835822@qq.com>
2021-05-28 15:00:16 +0800

    登录部分逻辑处理

---
+++ src/main/java/com/hzu/forum/forum/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package com.hzu.forum.forum.controller;
+
+import com.hzu.forum.forum.dto.AccessTokenDTO;
+import com.hzu.forum.forum.dto.GithubUser;
+import com.hzu.forum.forum.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    // 常量
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/hzu/forum/forum/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package com.hzu.forum.forum.dto;
+
+public class GithubUser {
+    private String login;
+    private long id;
+    private String bio;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
